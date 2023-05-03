ac955fc0a9dd8f88346db33962caff5eab3bad40
AdrianFrontend <adrian.kata.github@gmail.com>
2023-05-03 22:12:52 +0300

    release

--- script.js
+++ script.js
@@ -1,135 +1,23 @@
-let brands = [
-  lenovo = {
-    brandName: "Lenovo",
-    imgSrc: "./img/lenovo.svg",
-    hrefUrl: "#",
-    imgClass: "brand-card__lenovo"
-  },
-  samsung = {
-    brandName: "Samsung",
-    imgSrc: "./img/samsung.svg",
-    hrefUrl: "#",
-    imgClass: "brand-card__samsung"
-  },
-  apple = {
-    brandName: "Apple",
-    imgSrc: "./img/apple.svg",
-    hrefUrl: "#",
-    imgClass: "brand-card__apple"
-  },
-  viewSonic = {
-    brandName: "ViewSonic",
-    imgSrc: "./img/viewSonic.svg",
-    hrefUrl: "#",
-    imgClass: "brand-card__view-sonic"
-  },
-  bosch = {
-    brandName: "BOSCH",
-    imgSrc: "./img/bosch.svg",
-    hrefUrl: "#",
-    imgClass: "brand-card__bosch"
-  },
-  hp = {
-    brandName: "hp",
-    imgSrc: "./img/hp.svg",
-    hrefUrl: "#",
-    imgClass: "brand-card__hp"
-  },
-  acer = {
-    brandName: "Acer",
-    imgSrc: "./img/acer.svg",
-    hrefUrl: "#",
-    imgClass: "brand-card__acer"
-  },
-  sony = {
-    brandName: "SONY",
-    imgSrc: "./img/sony.svg",
-    hrefUrl: "#",
-    imgClass: "brand-card__sony"
-  },
-  lenovo = {
-    brandName: "Lenovo",
-    imgSrc: "./img/bosch.svg",
-    hrefUrl: "#",
-    imgClass: "brand-card__lenovo"
-  },
-  samsung = {
-    brandName: "Samsung",
-    imgSrc: "./img/samsung.svg",
-    hrefUrl: "#",
-    imgClass: "brand-card__samsung"
-  },
-  apple = {
-    brandName: "Apple",
-    imgSrc: "./img/apple.svg",
-    hrefUrl: "#",
-    imgClass: "brand-card__apple"
-  }
-]
-
-let windowWidth = window.innerWidth
-
-// if (windowWidth < 768) {
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
   direction: 'horizontal',
   loop: true,
-  // slidesPerView: 2,
   centeredSlides: true,
 
   breakpoints: {
-    650: {
+    620: {
       slidesPerView: 2
-    } 
+    },
   },
-
-  // If we need pagination
+  
   pagination: {
     el: '.swiper-pagination',
-    clickable: true,
-    // bulletClass: ""
+    clickable: true
   },
 });
-// }
-
-let swiperWrapper = document.querySelector(".swiper-wrapper")
-let slideTemplate = document.querySelector("#swiper__slide").content.querySelector(".swiper-slide")
-
-brands.map((brand) => {
-  let slide = slideTemplate.cloneNode(true)
-  let brandImg = slide.querySelector(".swiper-card__logo")
-  let brandButton = slide.querySelector(".swiper-card__button")
-
-  brandImg.src = brand.imgSrc
-  brandImg.alt = `Логотип ${brand.brandName}`
-  brandImg.classList.add(brand.imgClass)
-
-  brandButton.href = brand.hrefUrl
-
-  swiperWrapper.appendChild(slide)
-})
-
-
-
-let brandsList = document.querySelector(".brands-list")
-let brandTemplate = document.querySelector("#brands-list__item").content.querySelector(".brand-card")
-
-brands.map((brand) => {
-  let brandCard = brandTemplate.cloneNode(true)
-  let brandImg = brandCard.querySelector(".brand-card__logo")
-  let brandButton = brandCard.querySelector(".brand-card__button")
-
-  brandImg.src = brand.imgSrc
-  brandImg.alt = `Логотип ${brand.brandName}`
-  brandImg.classList.add(brand.imgClass)
-
-  brandButton.href = brand.hrefUrl
-
-  brandsList.appendChild(brandCard)
-})
 
 // Show all button
 
+let brandsList = document.querySelector(".brands-list")
 brandsListChildrens = brandsList.children
 
 let showAllButton = document.querySelector('.show-all-button')
@@ -144,12 +32,14 @@ showAllButton.addEventListener("click", () => {
   }
 })
 
+let windowWidth = window.innerWidth
 let hiddenElements = document.querySelectorAll(".brand-card")
 
 if (windowWidth <= 1015) {
   for (let i = 6; i < hiddenElements.length; i++) {
     hiddenElements[i].classList.add('brand-card--hidden')
   }
+
   showAllButton.addEventListener('click', (event) => {
     if (hiddenElements[6].classList.contains("brand-card--hidden")) {
       for (let i = 6; i < hiddenElements.length; i++) {
